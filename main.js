c6089cf547150045ed15e644bb79bb46c03e0af3
Andrew Kullman <agkullman@protonmail.com>
2023-10-24 12:08:59 -0800

    Commit 4 - Cleaned up code, add search buttons

--- main.js
+++ main.js
@@ -12,18 +12,21 @@ function start(){
 // Establishing map on page
 const map = L.map('map').setView([42.355, -71.050], 12);
 
-let Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-});
+let initMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-Stadia_AlidadeSmoothDark.addTo(map);
+initMap.addTo(map);
 
-// Set Marker Array
+// Set Marker Array and Search Storage
 
 const markersArray = []
+const searches = { 
+	lats:[], 
+	longs:[]
+}
+
+let searchIndex = searches.lats.length;
 
 // Event Listeners
 
@@ -33,21 +36,40 @@ function loadEventListeners() {
     const submit = document.querySelector("#submit");  // assuming the ID of the button is 'submit'
 	const addMarker = document.querySelector("#addMarker");
 	const deleteMarker = document.querySelector("#deleteMarker");
+	const previousSearch = document.querySelector('#previous');
+	const nextSearch = document.querySelector("#next");
 
     submit.addEventListener('click', latLongSearch);
+	lat.addEventListener('keydown', function(e){
+		if (e.which === 13 || e.keyCode === 13){
+			e.preventDefault();
+			latLongSearch();
+		}
+	})
 	addMarker.addEventListener('click', addMarkerFunc)
+	deleteMarker.addEventListener('click', deleteMarkerFunc)
+
+
+	previousSearch.addEventListener('click', function(){
+		searchIndex--
+		const previousLatSearch = searches.lats[searchIndex]
+		const previousLongSearch = searches.longs[searchIndex]
+		if (previousLatSearch.length === 0 || previousLongSearch.length === 0){
+			return;
+		}
+		map.setView([previousLatSearch,previousLongSearch],10)
+	})
 	
 }
 
+function deleteMarkerFunc(){
+	const markerToRemove = markersArray[0]; // This example just takes the first marker as an example
+	markerToRemove.remove(); // Removes from map
+	const index = markersArray.indexOf(markerToRemove);
+	if (index > -1) {
+    markersArray.splice(index, 1); // Removes from array
+}
 
-function latLongSearch(){
-	const latValue = parseFloat(lat.value);
-	const longValue = parseFloat(long.value);
-	if (!isNaN(latValue) && !isNaN(longValue)) {
-		map.setView([latValue, longValue],10);
-	} else {
-		alert("Please enter valid latitude and longitude values.");
-	}
 }
 
 function addMarkerFunc(){
@@ -59,7 +81,19 @@ function addMarkerFunc(){
 	});
 }
 
-
+function latLongSearch(){
+	const latValue = parseFloat(lat.value);
+	const longValue = parseFloat(long.value);
+	if (!isNaN(latValue) && !isNaN(longValue)) {
+		map.setView([latValue, longValue],10);
+		searches.lats.push(latValue);
+		searches.longs.push(longValue);
+		lat.value = '';
+		long.value = '';
+	} else {
+		alert("Please enter valid latitude and longitude values.");
+	}
+}
 
 
 map.on('mousemove', latLongDisplay);
