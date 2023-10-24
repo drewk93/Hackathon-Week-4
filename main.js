f940cd6e5d08c2a932d134d00fa922933180cdce
drewk93 <141455844+drewk93@users.noreply.github.com>
2023-10-24 08:15:41 -0800

    Commit 1 - Establishing Links and Basic Layout

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+
+// Establishing map on page
+var map = L.map('map').setView([51.505, -0.09], 15);
+
+    // OSM layer
+let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+})
+
+let Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
+
+Stadia_AlidadeSmoothDark.addTo(map);
